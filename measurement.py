7483db8c79d046ddf278420d45621de7fcc41eb5
John Bond <john.r.bond@gmail.com>
2014-03-06 11:13:11 -0500

    first commit

--- measurement.py
+++ measurement.py
@@ -20,7 +20,7 @@ def _cast(name, value, cast_type):
 class Measurement(object):
     '''new measuerment class'''
 
-    def __init__(self, description, target, address_family=4, resolve_on_probe=True, 
+    def __init__(self, description, target, probes, address_family=4, resolve_on_probe=True, 
             is_oneoff=True, can_visualise=True, is_public=True):
             self.description = _cast('description', description, str).lower()
             self.target = _cast('target', target, str).lower()
@@ -35,20 +35,78 @@ class Measurement(object):
 class MeasurementPing(Measurement):
     '''Ping measuerment class'''
 
-    def __init__(self, description, target, address_family=4, resolve_on_probe=True, 
+    def __init__(self, description, target, probes, address_family=4, resolve_on_probe=True, 
             is_oneoff=True, can_visualise=True, is_public=True,
             interval=240, packets=3, size=1):
-            super(MeasurementPing, self).__init__(description, target, address_family, 
+            super(MeasurementPing, self).__init__(description, target, probes, address_family, 
                     resolve_on_probe, is_oneoff, can_visualise, is_public)
+
             self.interval = _cast('interval', interval, int)
+            self.packets = _cast('packets', packets, int)
+            self.size = _cast('size', size, int)
+
             if self.interval < 1:
                 raise ValueError('interval ({}) must be a positive int'.format(self.interval))
-            self.packets = _cast('packets', packets, int)
 
             if self.packets < 1 or self.packets > 16:
                 raise ValueError('packets ({}) must be between 1 and 16'.format(self.packets))
 
-            self.size = _cast('size', size, int)
             if self.size < 1 or self.size > 2048:
                 raise ValueError('size ({}) must be between 1 and 2048'.format(self.size))
         
+class MeasurementTraceroute(Measurement):
+    '''Traceroute measuerment class'''
+
+    def __init__(self, description, target, probes, address_family=4, resolve_on_probe=True, 
+            is_oneoff=True, can_visualise=True, is_public=True,
+            interval=900, protocol='ICMP', dontfrag=False, paris=16, firsthop=1, maxhops=32, 
+            timeout=4000, size=40):
+            super(MeasurementTraceroute, self).__init__(description, target, probes, address_family, 
+                    resolve_on_probe, is_oneoff, can_visualise, is_public)
+
+            self.interval = _cast('interval', interval, int)
+            self.protocol = _cast('protocol', protocol, str)
+            self.dontfrag = _cast('dontfrag', dontfrag, bool)
+            self.firsthop = _cast('firsthop', firsthop, int)
+            self.maxhops = _cast('maxhops', maxhops, int)
+            self.timeout = _cast('timeout', timeout, int)
+            self.size = _cast('size', size, int)
+
+            if paris != None:
+                print paris
+                self.paris = _cast('paris', paris, int)
+                if self.paris < 1 or self.paris > 16:
+                    raise ValueError('paris ({}) must be between 1 and 16'.format(self.paris))
+
+            if self.interval < 1:
+                raise ValueError('interval ({}) must be a positive int'.format(self.interval))
+
+            if self.protocol not in ['ICMP', 'UDP']:
+                raise ValueError('protocol ({}) must be ICMP or UDP'.format(self.protocol))
+
+            if self.firsthop < 1 or self.firsthop > 255:
+                raise ValueError('firsthop ({}) must be between 1 and 255'.format(self.firsthop))
+        
+            if self.maxhops < 1 or self.maxhops > 255:
+                raise ValueError('maxhops ({}) must be between 1 and 255'.format(self.maxhops))
+        
+            if self.timeout < 1 or self.timeout > 60000:
+                raise ValueError('timeout ({}) must be between 1 and 60000'.format(self.timeout))
+        
+            if self.size < 1 or self.size > 2048:
+                raise ValueError('size ({}) must be between 1 and 2048'.format(self.size))
+        
+class MeasurementSSL(Measurement):
+    '''SSL measuerment class'''
+
+    def __init__(self, description, target, probes, address_family=4, resolve_on_probe=True, 
+            is_oneoff=True, can_visualise=True, is_public=True, 
+            port=443):
+            super(MeasurementSSL, self).__init__(description, target, probes, address_family, 
+                    resolve_on_probe, is_oneoff, can_visualise, is_public)
+
+            self.port = _cast('port', port, int)
+
+            if self.port < 1 or self.port > 65535:
+                raise ValueError('invalid port ({})'.format(self.port))
+
--- measurement_tests.py
+++ measurement_tests.py
@@ -1,5 +1,8 @@
 import unittest
+from probes import *
 from measurement import *
+
+probes= ProbesByAsn('42')
 
 class TestMeasurement(unittest.TestCase):
 
@@ -12,7 +15,7 @@ class TestMeasurement(unittest.TestCase):
         is_oneoff = True
         can_visualise = True
         is_public = True
-        measurement = Measurement(description, target, address_family, resolve_on_probe,
+        measurement = Measurement(description, target, probes, address_family, resolve_on_probe,
             is_oneoff, can_visualise, is_public)
         self.assertIsInstance(measurement, Measurement)
 
@@ -25,7 +28,7 @@ class TestMeasurement(unittest.TestCase):
         is_oneoff = 'true'
         can_visualise = 'False'
         is_public = 'false'
-        measurement = Measurement(description, target, address_family, resolve_on_probe,
+        measurement = Measurement(description, target, probes, address_family, resolve_on_probe,
             is_oneoff, can_visualise, is_public)
         self.assertIsInstance(measurement, Measurement)
 
@@ -38,7 +41,7 @@ class TestMeasurement(unittest.TestCase):
         is_oneoff = 1
         can_visualise = '0'
         is_public = 0
-        measurement = Measurement(description, target, address_family, resolve_on_probe,
+        measurement = Measurement(description, target, probes, address_family, resolve_on_probe,
             is_oneoff, can_visualise, is_public)
         self.assertIsInstance(measurement, Measurement)
 
@@ -51,7 +54,7 @@ class TestMeasurement(unittest.TestCase):
         is_oneoff = True
         can_visualise = True
         is_public = True
-        self.failUnlessRaises(ValueError, Measurement, description, target, address_family, 
+        self.failUnlessRaises(ValueError, Measurement, description, target, probes, address_family, 
                 resolve_on_probe, is_oneoff, can_visualise, is_public)
 
     def testBadMeasurement_2(self):
@@ -63,25 +66,184 @@ class TestMeasurement(unittest.TestCase):
         is_oneoff = True
         can_visualise = True
         is_public = True
-        self.failUnlessRaises(ValueError, Measurement, description, target, address_family, 
+        self.failUnlessRaises(ValueError, Measurement, description, target, probes, address_family, 
                 resolve_on_probe, is_oneoff, can_visualise, is_public)
 
     def testGoodMeasurementPing_1(self):
         '''Test a valid Ping Measurement'''
         description = 'test'
         target = 'test'
-        address_family = 4
-        resolve_on_probe = True
-        is_oneoff = True
-        can_visualise = True
-        is_public = True
-        interval=240
-        packets=3
-        size=1
-        measurement = MeasurementPing(description, target, address_family, resolve_on_probe,
-            is_oneoff, can_visualise, is_public, interval, packets, size)
+        measurement = MeasurementPing(description, target, probes)
         self.assertIsInstance(measurement, MeasurementPing)
+
+    def testBadMeasurementPing_1(self):
+        '''Test bad interval'''
+        description = 'test'
+        target = 'test'
+        interval = 0
+        self.failUnlessRaises(ValueError, MeasurementPing, description, target, probes, 
+                interval=interval)
+
+    def testBadMeasurementPing_2(self):
+        '''Test 0 packet count'''
+        description = 'test'
+        target = 'test'
+        packets = 0
+        self.failUnlessRaises(ValueError, MeasurementPing, description, target, probes, 
+                packets=packets)
+
+    def testBadMeasurementPing_3(self):
+        '''Test high packet count'''
+        description = 'test'
+        target = 'test'
+        packets = 17
+        self.failUnlessRaises(ValueError, MeasurementPing, description, target, probes, 
+                packets=packets)
+
+    def testBadMeasurementPing_4(self):
+        '''Test 0 size'''
+        description = 'test'
+        target = 'test'
+        size = 0
+        self.failUnlessRaises(ValueError, MeasurementPing, description, target, probes, 
+                size=size)
+
+    def testBadMeasurementPing_5(self):
+        '''Test high size'''
+        description = 'test'
+        target = 'test'
+        size = 2049
+        self.failUnlessRaises(ValueError, MeasurementPing, description, target, probes, 
+                size=size)
+
+    def testGoodMeasurementSSL_1(self):
+        '''Test a valid SSL Measurement'''
+        description = 'test'
+        target = 'test'
+        measurement = MeasurementSSL(description, target, probes)
+        self.assertIsInstance(measurement, MeasurementSSL)
+
+    def testBadMeasurementSSL_1(self):
+        '''Test low port'''
+        description = 'test'
+        target = 'test'
+        port = 0
+        self.failUnlessRaises(ValueError, MeasurementSSL, description, target, probes, 
+                port=port)
+
+    def testBadMeasurementSSL_2(self):
+        '''Test high port'''
+        description = 'test'
+        target = 'test'
+        port = 605536
+        self.failUnlessRaises(ValueError, MeasurementSSL, description, target, probes, 
+                port=port)
+
+    def testGoodMeasurementTraceroute_1(self):
+        '''Test a valid Traceroute Measurement'''
+        description = 'test'
+        target = 'test'
+        measurement = MeasurementTraceroute(description, target, probes)
+        self.assertIsInstance(measurement, MeasurementTraceroute)
+
+    def testBadMeasurementTraceroute_1(self):
+        '''Test bad interval'''
+        description = 'test'
+        target = 'test'
+        interval = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                interval=interval)
+
+    def testBadMeasurementTraceroute_2(self):
+        '''Test bad protocol'''
+        description = 'test'
+        target = 'test'
+        protocol = 'TCP'
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                protocol=protocol)
+
+    def testBadMeasurementTraceroute_3(self):
+        '''Test 0 paris count'''
+        description = 'test'
+        target = 'test'
+        paris = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                paris=paris)
+
+    def testBadMeasurementTraceroute_4(self):
+        '''Test high paris count'''
+        description = 'test'
+        target = 'test'
+        paris = 17
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                paris=paris)
+
+    def testBadMeasurementTraceroute_5(self):
+        '''Test 0 firsthop count'''
+        description = 'test'
+        target = 'test'
+        firsthop = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                firsthop=firsthop)
+
+    def testBadMeasurementTraceroute_6(self):
+        '''Test high firsthop count'''
+        description = 'test'
+        target = 'test'
+        firsthop = 256
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                firsthop=firsthop)
+
+    def testBadMeasurementTraceroute_7(self):
+        '''Test 0 maxhops count'''
+        description = 'test'
+        target = 'test'
+        maxhops = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                maxhops=maxhops)
+
+    def testBadMeasurementTraceroute_8(self):
+        '''Test high maxhops count'''
+        description = 'test'
+        target = 'test'
+        maxhops = 256
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                maxhops=maxhops)
+
+    def testBadMeasurementTraceroute_9(self):
+        '''Test 0 timeout'''
+        description = 'test'
+        target = 'test'
+        timeout = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                timeout=timeout)
+
+    def testBadMeasurementTraceroute_10(self):
+        '''Test high timeout'''
+        description = 'test'
+        target = 'test'
+        timeout = 60001
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                timeout=timeout)
+
+    def testBadMeasurementTraceroute_11(self):
+        '''Test 0 size'''
+        description = 'test'
+        target = 'test'
+        size = 0
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                size=size)
+
+    def testBadMeasurementTraceroute_12(self):
+        '''Test high size'''
+        description = 'test'
+        target = 'test'
+        size = 2049
+        self.failUnlessRaises(ValueError, MeasurementTraceroute, description, target, probes, 
+                size=size)
+
 
 if __name__ == '__main__':
     unittest.main()
+
 
